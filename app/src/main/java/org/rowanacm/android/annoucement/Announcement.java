e24489b907f541157b740e3563a69477f1172bea
tylercarberry <tylercarberry@gmail.com>
2016-12-07 21:48:28 -0500

    Move annoucements to own package

---
+++ app/src/main/java/org/rowanacm/android/annoucement/Announcement.java
@@ -0,0 +1,37 @@
+package org.rowanacm.android.annoucement;
+
+import java.io.Serializable;
+
+/**
+ * An message by Rowan ACM.
+ * Contains a message and committee
+ */
+
+public class Announcement implements Serializable {
+    private String committee = "";
+    private String message = "";
+
+    public Announcement() {
+    }
+
+    public Announcement(String committee, String message) {
+        this.committee = committee;
+        this.message = message;
+    }
+
+    public String getCommittee() {
+        return committee;
+    }
+
+    public void setCommittee(String committee) {
+        this.committee = committee;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
